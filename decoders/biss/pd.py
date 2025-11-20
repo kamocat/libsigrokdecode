eeaaf37d0b58b9ff8b34df04c8152dff7d97f555
Marshal Horn <kamocat@gmail.com>
2025-11-20 07:28:22 -0800

    BISS: Implemented CRC check

--- decoders/biss/pd.py
+++ decoders/biss/pd.py
@@ -1,5 +1,14 @@
 import sigrokdecode as srd
 
+def calc_crc(quotient, bits, poly):
+    expected = quotient & 0x3F
+    quotient -= expected
+    for x in range(bits-7, -1, -1):
+        if quotient & (0x40 << x):
+            quotient ^= poly << x
+    res = quotient & 0x3F
+    res ^= 0x3F
+    return (res, expected)
 
 class Decoder(srd.Decoder):
     api_version = 3
@@ -18,7 +27,7 @@ class Decoder(srd.Decoder):
     #optional_channels = ({"id": "mo", "name": "MO", "desc": "Actuator data"},)
     options = (
         {"id": "d_len", "description": "data length", "default": 32},
-        {"id": "crc", "description": "CRC polynomial", "default": 0x43},
+        {"id": "poly", "description": "CRC polynomial", "default": 0x43},
     )
     annotations = (
         ("bit", "data bit"),  # 0
@@ -154,8 +163,8 @@ class Decoder(srd.Decoder):
                     self.put(clock_edges[2+self.d_len], clock_edges[3+self.d_len], self.out_ann, [11, ['Error', 'Err', 'E']])
                 if not bits[self.d_len+3]:
                     self.put(clock_edges[3+self.d_len], clock_edges[4+self.d_len], self.out_ann, [12, ['Warning', 'Warn', 'W']])
-                crc = 0x3F & (data_word>>(bitcount-self.d_len-8))
-                self.put(clock_edges[4+self.d_len], clock_edges[10+self.d_len], self.out_ann, [9, [f'{crc:02X}']])
+                expected, crc = calc_crc(data_word>>(bitcount-self.d_len-8), self.d_len+8, self.options['poly'])
+                self.put(clock_edges[4+self.d_len], clock_edges[10+self.d_len], self.out_ann, [9 if expected==crc else 13, [f'{expected:02X}']])
             self.put(clock_edges[2], clock_edges[-1], self.out_binary, [0, data_word.to_bytes(-(-bitcount//8), byteorder='big')])
             timeout = clock_edges[-1]
             self.put(
